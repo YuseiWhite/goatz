42f392399b9e2932125d2d32a224c175059651c9
yuseirf <yusei.tanaka2112325@icloud.com>
2021-07-03 04:31:55 +0900

    7-5で勝利可能に

--- game_app/models/six_games.py
+++ game_app/models/six_games.py
@@ -1,42 +1,3 @@
-"""
-【1セットマッチ】
-
-6ゲーム取ったら勝利。
-1ゲームは 0 → 15 → 30 → 40 → 1ゲーム という順。
-
-if文で
-数字 % 5 == 0
-数字 % 4 == 0
-数字 % 3 == 0
-数字 % 2 == 0
-数字 % 1 == 0
-
-gameは全て5の倍数になる
-つまり、プレイヤーのカウントが6ゲームまでで5, 10, 15, 20, 25, 30...となる。
-1ゲーム　＝　5
-→　数字 % 5 == 0
-2ゲーム　＝　10
-→　数字 % 5 == 5, 数字 % 10 == 0
-3ゲーム　＝　15
-...
-6ゲーム　＝　30
-
-
-5-5なら2点差着くまで
-このゲームを6つ先に取ったら勝利。
-
-ただし、以下の場合は異なる。
-・40-40となった場合
-　先に2ポイント連続で取ったものが1ゲーム。40の次はAd
-
-・5-5となった場合
-　6-5で勝利ではなく、7-5となるまでやらなくてはならない。
-
-・6-6となった場合
-　タイブレーク実行で買ったものが勝利者。
-　例えば、7-3のポイントで買った場合は 7-6(3) と表記する
-
-"""
 import pdb
 
 from match import AboutPlayers
@@ -190,11 +151,9 @@ class SixGame(HowToCountGame):
     def six_games(self):
         how_to_count_game = HowToCountGame()
         while True:
-            game_of_two_players = player1_name + ": " + str(player1_game_count) \
-                                  + "\n" + player2_name + ": " + str(player2_game_count)
+            game_of_two_players = player1_name + ": " + str(player1_game_count) + "\n" + player2_name + ": " + str(player2_game_count)
             # タイブレークの処理
-            if (player1_game_count != 0 and player1_game_count % 5 == 0) \
-                    and (player2_game_count != 0 and player2_game_count % 5 == 0):
+            if (player1_game_count != 0 and player1_game_count % 5 == 0) and (player2_game_count != 0 and player2_game_count % 5 == 0):
                 while True:
                     if player1_game_count % 6 == 0 and player2_game_count % 6 == 0:
                         while True:
@@ -212,17 +171,17 @@ class SixGame(HowToCountGame):
                                 break
                             tiebreak = SevenPointsTieBreak()
                             tiebreak.seven_points_tie_break()
-                            return about.player1, about.player2
                     elif player1_game_count == 7 or player2_game_count == 7:
                         print("#######################################################")
-                        print(game_of_two_players)
+                        print(player1_name + ": " + str(player1_game_count) + "\n" + player2_name + ": " + str(player2_game_count))
                         print("#######################################################")
                         break
+                    print("#######################################################")
+                    print(player1_name + ": " + str(player1_game_count) + "\n" + player2_name + ": " + str(player2_game_count))
+                    print("#######################################################")
                     how_to_count_game.game_count()
-                    print("#######################################################")
-                    print(game_of_two_players)
-                    print("#######################################################")
-
+            if player1_game_count > 6 or player2_game_count > 6:
+                break
             # プレイヤー1とプレイヤー2の処理
             elif player1_game_count != 0 and player1_game_count % 6 == 0:
                 print("#######################################################")
