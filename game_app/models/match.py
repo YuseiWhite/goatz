bd2d15479b71b5cd06e81f98b8e89efb8757e7d2
yuseirf <yusei.tanaka2112325@icloud.com>
2021-07-20 01:09:24 +0900

    グローバル変数をmatch.pyのabout()に定義

--- game_app/models/match.py
+++ game_app/models/match.py
@@ -1,4 +1,5 @@
 class AboutPlayers(object):
+    # 全てのマッチで使用
     player1_name = "Aさん"
     player2_name = "Bさん"
     which_point = "1:" + player1_name + "がポイントを取った\n" + "2:" + player2_name + "がポイントを取った\n1か2で入力して下さい："
@@ -6,3 +7,9 @@ class AboutPlayers(object):
     player2 = 0
     win_player1 = "Game set and match won by " + player1_name + "."
     win_player2 = "Game set and match won by " + player2_name + "."
+    # ゲームマッチで使用
+    player1_count = 1
+    player2_count = 1
+    players_count = {1: "0", 2: "15", 3: "30", 4: "40", 5: "game"}
+    player1_game_count = 0
+    player2_game_count = 0
