30ebec8c392bd603d61429b937b427fb47be5ed7
yuseirf <yusei.tanaka2112325@icloud.com>
2021-08-29 08:45:30 +0900

    completed GUI of apps.py

--- game_app/controller/apps.py
+++ game_app/controller/apps.py
@@ -3,11 +3,23 @@
 import os
 import sys
 sys.path.append(os.path.join('..', 'models'))
+import json
+
+from pywebio.input import input, TEXT, NUMBER, select, checkbox, radio, textarea, file_upload, input_group
+from pywebio.output import put_text, put_buttons, put_link, put_markdown, put_table, put_image, popup
+from pywebio.session import hold
 
 from game_app.models.eight_games import EightGames
 from game_app.models.tiebreak import SevenPointsTieBreak, TenPointsTieBreak
 from game_app.models.six_games import SixGames
 from game_app.models.three_sets import ThreeSets
+
+replace_table = str.maketrans({
+    ",": "\n",
+    "{": "",
+    "}": "",
+    '"': ''
+})
 
 
 class StartGame(object):
@@ -15,31 +27,43 @@ class StartGame(object):
         pass
 
     def input_name(self):
-        separation = "#" * 55
-        start = separation + "\n試合開始！\n" + separation
-        print(start)
-        player1_name = input(u"あなたの名前を入力して下さい：")
+        start = "テニスカウントアプリケーション「Goatz」へようこそ！\nさあ、試合開始です！\nテニスを楽しみましょう！"
+        put_text(start)
+        player1_name = input("あなたの名前を入力して下さい：", type=TEXT, required=True)
         player1_name = player1_name + "さん"
-        player2_name = input(u"相手の名前を入力して下さい：")
+        player2_name = input("相手の名前を入力して下さい：", type=TEXT, required=True)
         player2_name = player2_name + "さん"
         return player1_name, player2_name
 
     def select_game_format(self):
-        separation = "#" * 55
         while True:
-            select_game_format = input(
-                separation +
-                "\n1： 6ゲームマッチ\n"
-                "2： 8ゲームマッチ\n"
-                "3： タイブレークマッチ\n"
-                "4： スーパータイブレークマッチ\n"
-                "5:　3セットマッチ\n"
-                + separation +
-                "\n試合形式を選択して下さい："
-            )
-            if select_game_format in ["1", "2", "3", "4", "5"]:
-                select_game_format = int(select_game_format)
-                return select_game_format
+            game_format_list = {
+                1: " 1： 6ゲームマッチ",
+                2: "2： 8ゲームマッチ",
+                3: "3： タイブレークマッチ",
+                4: "4： スーパータイブレークマッチ",
+                5: "5： 3セットマッチ"
+            }
+            str_game_format_list = json.dumps(game_format_list, ensure_ascii=False)
+            put_text(str_game_format_list.translate(replace_table).replace("1:", "").replace("2:", "").replace("3:", "").replace("4:", "").replace("5:", ""))
+            game_format_list_num = ["1番", "2番", "3番", "4番", "5番"]
+            select_game_format = radio("試合形式を選択して下さい：", options=game_format_list_num)
+            if select_game_format == "1番":
+                select_game_format = 1
+                popup(game_format_list[1] + "という試合形式で試合開始です！", size="large")
+            elif select_game_format == "2番":
+                select_game_format = 2
+                popup(game_format_list[2] + "という試合形式で試合開始です！", size="large")
+            elif select_game_format == "3番":
+                select_game_format = 3
+                popup(game_format_list[3] + "という試合形式で試合開始です！", size="large")
+            elif select_game_format == "4番":
+                select_game_format = 4
+                popup(game_format_list[4] + "という試合形式で試合開始です！", size="large")
+            elif select_game_format == "5番":
+                select_game_format = 5
+                popup(game_format_list[5] + "という試合形式で試合開始です！", size="large")
+            return select_game_format
 
     def start_game(self):
         separation = "#" * 55
@@ -50,17 +74,22 @@ class StartGame(object):
             if game_format_num == 1:
                 six_games = SixGames(player1_name, player2_name)
                 six_games.run_six_games_match()
+                hold()
             elif game_format_num == 2:
                 eight_games = EightGames(player1_name, player2_name)
                 eight_games.run_eight_games_match()
+                hold()
             elif game_format_num == 3:
                 tiebreak = SevenPointsTieBreak(player1_name, player2_name)
                 tiebreak.seven_points_tie_break_result()
+                hold()
             elif game_format_num == 4:
                 super_tiebreak = TenPointsTieBreak(player1_name, player2_name)
                 super_tiebreak.ten_points_tie_break_result()
+                hold()
             elif game_format_num == 5:
                 three_sets = ThreeSets(player1_name, player2_name)
                 three_sets.run_three_sets_match()
+                hold()
 
 
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from pywebio.platform.tornado import start_server
+from pywebio.session import hold
+
 from game_app.controller.apps import StartGame
 
 
 def main():
     app = StartGame()
     app.start_game()
+    start_server(main, port=8000)
+    hold()
 
 
 if __name__ == '__main__':
