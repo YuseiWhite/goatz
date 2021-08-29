aeabcad65e533892b4608ebf836b854234d00b4c
yuseirf <yusei.tanaka2112325@icloud.com>
2021-08-29 09:48:41 +0900

    completed GUI of tiebreak.py

--- game_app/controller/apps.py
+++ game_app/controller/apps.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.join('..', 'models'))
 import json
 
 from pywebio.input import input, TEXT, NUMBER, select, checkbox, radio, textarea, file_upload, input_group
-from pywebio.output import put_text, put_buttons, put_link, put_markdown, put_table, put_image, popup
+from pywebio.output import put_text, put_buttons, put_link, put_markdown, put_table, put_image, popup, toast
 from pywebio.session import hold
 
 from game_app.models.eight_games import EightGames
@@ -50,27 +50,26 @@ class StartGame(object):
             select_game_format = radio("試合形式を選択して下さい：", options=game_format_list_num)
             if select_game_format == "1番":
                 select_game_format = 1
-                popup(game_format_list[1] + "という試合形式で試合開始です！", size="large")
+                toast(game_format_list[1] + "という試合形式で試合開始です！", duration=3, position='left')
             elif select_game_format == "2番":
                 select_game_format = 2
-                popup(game_format_list[2] + "という試合形式で試合開始です！", size="large")
+                toast(game_format_list[2] + "という試合形式で試合開始です！", duration=3, position='left')
             elif select_game_format == "3番":
                 select_game_format = 3
-                popup(game_format_list[3] + "という試合形式で試合開始です！", size="large")
+                toast(game_format_list[3] + "という試合形式で試合開始です！", duration=3, position='left')
             elif select_game_format == "4番":
                 select_game_format = 4
-                popup(game_format_list[4] + "という試合形式で試合開始です！", size="large")
+                toast(game_format_list[4] + "という試合形式で試合開始です！", duration=3, position='left')
             elif select_game_format == "5番":
                 select_game_format = 5
-                popup(game_format_list[5] + "という試合形式で試合開始です！", size="large")
+                toast(game_format_list[5] + "という試合形式で試合開始です！", duration=3, position='left')
             return select_game_format
 
     def start_game(self):
-        separation = "#" * 55
         player1_name, player2_name = self.input_name()
         game_format_num = self.select_game_format()
         if 1 <= game_format_num <= 5:
-            print(separation + "\nLove all.\n" + separation)
+            popup("Love all.")
             if game_format_num == 1:
                 six_games = SixGames(player1_name, player2_name)
                 six_games.run_six_games_match()
